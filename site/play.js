e6f92842a200d172c1924d02e67a001e340e34a7
rerot <rerot@rerot-HP-EliteBook-8540w.(none)>
2014-02-02 11:47:28 +0100

    mise en relation des images d'Ahmed et avec le plateau. Done

--- site/play.js
+++ site/play.js
@@ -1,63 +1,47 @@
 window.onload = function() 
 {
     var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update });
-       
-    var p1 = 
-	{ 
-	    name:"p1",
-	    x:44,
-	    y:54,
-	    pos:1,
-	    url:"game01.html"
-	}
-    
-    var p2 = 
-	{ 
-	    name:"p2",
-	    x:269,
-	    y:286,
-	    pos:2,
-	    url:"game01.html"
-	}
 
-    var p3 = 
-	{ 
-	    name:"p3",
-	    x:449,
-	    y:286,
-	    pos:3,
-	    url:"game01.html"
-	}
-    
-    var p4 = 
-	{ 
-	    name:"p4",
-	    x:635,
-	    y:158,
-	    pos:4,
-	    url:"game01.html"
-	}
-
-    var marge = 0;
-    var begin = { x:44, y:54 };
+    var level = function(name, x, y, pos, url, score) 
+    {
+	this.name = name;
+	this.x = x;
+	this.y = y;
+	this.pos = pos;
+	this.url = url;
+	this.score = 0;
+	//this.score = return_score(name, pos);
+    };
+ 
+    var p1 = new level("p1", 20, 300, 1, "../game01.html");
+    var p2 = new level("p2", 80, 340, 2, "../game01.html");
+    var p3 = new level("p3", 155, 365, 3, "../game01.html");
+    var p4 = new level("p4", 230, 385, 4, "../game01.html");       
+    var p5 = new level("p5", 305, 400, 5, "../game01.html");
+    var p6 = new level("p6", 380, 410, 6, "../game01.html");
+    var p7 = new level("p7", 460, 420, 7, "../game01.html");
+    var p8 = new level("p8", 535, 420, 8, "../game01.html");
+    var p9 = new level("p9", 610, 400, 9, "../game01.html");
+    var p10 = new level("p10", 680, 370, 10, "../game01.html");
+    var b1 = new level("b1", 305, 320, 11, "../game01.html");
+    var b2 = new level("b2", 535, 340, 12, "../game01.html");
+    var beginmap = false;
+    var marge = 10;
+    var begin = { x:20, y:300 };
     var texture_group;
-    var marge = { x:20, y:20};
-    var target = { x:100, y:100};
+    var marge = { x:30, y:27};
+    var target = { x:80, y:340};
     var mummy;
-    speed = 200;
-    iter = 0;
-    start = false;
-    var all_target = [p1, p2, p3, p4];
-    var begin = { x:44, y:54 };
-    pos = 1;
+    var all_target = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, b1, b2];
     var logo;
     var buttonjouer;
     var buttoncredits;
     var background;
     var music;
-
-
-
+    speed = 100;
+    iter = 0;
+    start = false;
+    pos = 1;
     
     function preload() 
     {
@@ -109,47 +93,47 @@ window.onload = function()
 
     function actionOnClickJouer () 
     {
-
+	beginmap = true;
 	texture_group = game.add.group();
 	texture_group.create(0, 0, 'map');
+	game.input.onDown.add(detect, this);
+
+	/*level button*/
+	button1 = game.add.button(20, 300, 'button1', actionOnClicklevel1, this, 2,1,0);
+	button2 = game.add.button(80, 340, 'button2', actionOnClicklevel2, this, 2,1,0);
+	button3 = game.add.button(155, 365, 'button3', actionOnClicklevel3, this, 2,1,0);
+	button4 = game.add.button(230, 385, 'button4', actionOnClicklevel4, this, 2,1,0);
+	button5 = game.add.button(305, 400, 'button5', actionOnClicklevel5, this, 2,1,0);
+  	button6 = game.add.button(380, 410, 'button6', actionOnClicklevel6, this, 2,1,0);
+	button7 = game.add.button(460, 420, 'button7', actionOnClicklevel7, this, 2,1,0);
+	button8 = game.add.button(535, 420, 'button8', actionOnClicklevel8, this, 2,1,0);
+	button9 = game.add.button(610, 400, 'button9', actionOnClicklevel9, this, 2,1,0);
+	button10 = game.add.button(680, 370, 'button10', actionOnClicklevel10, this, 2,1,0);
+	bonus1 = game.add.button(305, 320, 'bonus1', actionOnClickbonus1, this, 2,1,0);
+	bonus2 = game.add.button(535, 340, 'bonus2', actionOnClickbonus2, this, 2,1,0);
+	
+	/*mommie*/
 	mummy = game.add.sprite((begin.x), (begin.y), 'mummy');
 	mummy.animations.add('play');
 	mummy.animations.play('play', 30, true);
-	game.input.onDown.add(detect, this);
+
 	move_pos();
-
-	/*level button*/
-     button1 = game.add.button(20, 300, 'button1', actionOnClicklevel1, this, 2,1,0);
-     button2 = game.add.button(80, 340, 'button2', actionOnClicklevel2, this, 2,1,0);
-     button3 = game.add.button(155, 365, 'button3', actionOnClicklevel3, this, 2,1,0);
-     button4 = game.add.button(230, 385, 'button4', actionOnClicklevel4, this, 2,1,0);
-     button5 = game.add.button(305, 400, 'button5', actionOnClicklevel5, this, 2,1,0);
-  	 button6 = game.add.button(380, 410, 'button6', actionOnClicklevel6, this, 2,1,0);
-     button7 = game.add.button(460, 420, 'button7', actionOnClicklevel7, this, 2,1,0);
-     button8 = game.add.button(535, 420, 'button8', actionOnClicklevel8, this, 2,1,0);
-     button9 = game.add.button(610, 400, 'button9', actionOnClicklevel9, this, 2,1,0);
-	 button10 = game.add.button(680, 370, 'button10', actionOnClicklevel10, this, 2,1,0);
-	 bonus1 = game.add.button(305, 320, 'bonus1', actionOnClickbonus1, this, 2,1,0);
-	 bonus2 = game.add.button(535, 340, 'bonus2', actionOnClickbonus2, this, 2,1,0);
-
-	/*/
-    
-    /*animation notes*/
+	
+	/*animation notes*/
 	var emitter = game.add.emitter(120, 500, 0);
-    emitter.makeParticles('balls', [0, 1, 2, 3, 4, 5, 6]);
-    emitter.minParticleSpeed.setTo(-100, -100);
-    emitter.maxParticleSpeed.setTo(100, 100);
-    emitter.gravity = 0;
-    emitter.start(false, 4000, 0);
-    /**/
-
+	emitter.makeParticles('balls', [0, 1, 2, 3, 4, 5, 6]);
+	emitter.minParticleSpeed.setTo(-100, -100);
+	emitter.maxParticleSpeed.setTo(100, 100);
+	emitter.gravity = 0;
+	emitter.start(false, 4000, 0);
+	/**/
+	
 	buttonjouer.kill();
    	buttoncredits.kill();
-   	}
+    }
     
     function actionOnClickCredits () 
     {
-	  
 	  texture_group = game.add.group();
 	  texture_group.create(0, 0, 'credits');
 	  buttonjouer.kill();
@@ -157,24 +141,30 @@ window.onload = function()
    	  var back = game.add.button(40, 500, 'back', backmenu, this, 2, 1, 0);
     }
 
-    function backmenu(){
+    function backmenu()
+    {
     	window.location.reload();
     }
     
     function return_elem(pos)
     {
-		for (var i = 0; i < all_target.length; i++) 
-		{
+	for (var i = 0; i < all_target.length; i++) 
+	{
 	    if (all_target[i].pos == pos)
 		return all_target[i];
-		}
-		return null;
+	}
+	return null;
     }
     
     function relation_match(target_pos)
     {
+	//alert("pos " + pos + " | target_pos " + target_pos);
 	if (target_pos == 0 || pos == 0)
 	    return false;
+	if ((pos == 5 && target_pos == 11) || pos == 11 && target_pos == 5)
+	    return true;
+	if ((pos == 8 && target_pos == 12) || pos == 12 && target_pos == 8)
+	    return true;
 	if ((pos + 1 == target_pos) || (pos - 1 == target_pos))
 	    return true;
 	return false;
@@ -198,8 +188,11 @@ window.onload = function()
      
     function match_all_target(pos_x, pos_y)
     {
+	pouet = true;
 	for (var i = 0; i < all_target.length; i++) 
 	{
+	    //alert("pos_x = " + pos_x + " | pos_y = " + pos_y);
+	    //alert("ma pos est " + pos + " et je rentre avec " + i);
 	    if (match_one_target(all_target[i].x, all_target[i].y, pos_x, pos_y) == true)
 		return true;
 	}
@@ -216,17 +209,20 @@ window.onload = function()
 	    return true;
 	return false;						   
     }
-     
+    
     function update()
     {
-	if (iter == speed)
+	if (iter == speed || beginmap == true)
 	{
 	    pos = return_pos(mummy.x, mummy.y);
 	    if (game.input.keyboard.isDown(Phaser.Keyboard.ENTER))
 	    {
-		temp = return_elem(pos).url + "?ts=";
-		alert("Bienvenue dans le niveau " + pos);
-		window.location.href = temp + new Date().getMilliseconds();
+		temp = return_elem(pos);
+		if (temp != null)
+		{
+		    alert("Bienvenue dans le niveau " + pos);
+		    window.location.href = (temp.url + "?ts=") + new Date().getMilliseconds();
+		}
 	    }
 	}
 	if (iter < speed && start == true)
@@ -236,78 +232,88 @@ window.onload = function()
             iter++;
 	}
     }
-
+    
     function render() 
     {
     }
     
     function detect()
     {
-	var i = parseInt(game.input.x);
-	var j = parseInt(game.input.y);
-	if ((iter == speed || iter == 0) && match_all_target(i, j) == true)
+    }
+    
+    function move_player(level)
+    {
+	if (return_pos(mummy.x, mummy.y) != 0)
 	{
-	    if (return_pos(mummy.x, mummy.y) != 0 && relation_match(return_pos(i, j)) == true)
+	    if (relation_match(level.pos) == true)
 	    {
 		start = true;
-		target.x = i;
-		target.y = j;
+		target.x = level.x;
+		target.y = level.y;
 		move_pos();
 		iter = 0;
 	    }
 	}
     }
-
-    function is_moving()
-    {
-	if (iter != 0 && start == true && iter < speed)
-	{
-	    mummy.animations.play('play', 30, true);
-	    return true;					 
-	}
-	else
-	{
-	    mummy.animations.add('stop');
-	    return false;					 
-	}
-    }
-
+    
     /*Function level*/
-     function actionOnClicklevel1() {
-
-    }
-     function actionOnClicklevel2() {
-
-    }
-     function actionOnClicklevel3() {
-
-    }
-     function actionOnClicklevel4() {
-
-    }
-     function actionOnClicklevel5() {
-
-    }
-     function actionOnClicklevel6() {
-
-    }
-     function actionOnClicklevel7() {
-
-    }
-     function actionOnClicklevel8() {
-
-    }
-     function actionOnClicklevel9() {
-
-    }
-     function actionOnClicklevel10() {
-
-    }
-     function actionOnClickbonus1() {
-
-    }
-      function actionOnClickbonus2() {
-
+    function actionOnClicklevel1() 
+    {
+	move_player(p1);
     }
 
+    function actionOnClicklevel2() 
+    {
+	move_player(p2);
+    }
+
+    function actionOnClicklevel3() 
+    {
+	move_player(p3);
+    }
+
+    function actionOnClicklevel4() 
+    {
+	move_player(p4);
+    }
+    
+    function actionOnClicklevel5() 
+    {
+	move_player(p5);
+    }
+    
+    function actionOnClicklevel6() 
+    {
+	move_player(p6);
+    }
+    
+    function actionOnClicklevel7() 
+    {
+	move_player(p7);
+    }
+    
+    function actionOnClicklevel8() 
+    {
+	move_player(p8);
+    }
+    
+    function actionOnClicklevel9() 
+    {
+	move_player(p9);
+    }
+    
+    function actionOnClicklevel10() 
+    {
+	move_player(p10);
+    }
+    
+    function actionOnClickbonus1() 
+    {
+	move_player(b1);
+    }
+    
+    function actionOnClickbonus2() 
+    {
+	move_player(b2);
+    }
 }
